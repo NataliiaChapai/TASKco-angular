1e1caad69c8a040a06b5bb9b692ec3d84769a4e1
Nataliia Chapai <natalya.chapay@gmail.com>
2022-10-13 14:06:06 +0300

    optimize sort pipe

--- src/app/shared/pipes/sort/sort.pipe.ts
+++ src/app/shared/pipes/sort/sort.pipe.ts
@@ -8,41 +8,24 @@ import { Board } from 'src/app/dashboard/models/board.interface';
 export class SortPipe implements PipeTransform {
 
   transform(list: Board[] | null, sort: string): any {
-    if (sort === 'name' && list) {
+    
+    if (list) {
+      if (sort === 'name'|| sort === 'createdAt') {
       list.sort((a, b) => {
-        if ( a.name === b.name ) {
+        if ( a[sort] === b[sort] ) {
         return 0;
         }
-      return( ( a.name < b.name ) ? -1 : 1 )}
+      return( ( a[sort] < b[sort] ) ? -1 : 1 )}
     )}
-    if (sort === 'date' && list) {
+    
+    if (sort === 'todo' || sort === 'inprogress' || sort === 'done') {
       list.sort((a, b) => {
-        if ( a.createdAt === b.createdAt ) {
+        if ( a.taskCount[sort] === b.taskCount[sort] ) {
         return 0;
         }
-      return( ( a.createdAt < b.createdAt ) ? -1 : 1 )}
+      return( ( a.taskCount[sort] < b.taskCount[sort] ) ? -1 : 1 )}
     )}
-    if (sort === 'todo' && list) {
-      list.sort((a, b) => {
-        if ( a.taskCount.todo === b.taskCount.todo ) {
-        return 0;
-        }
-      return( ( a.taskCount.todo < b.taskCount.todo ) ? -1 : 1 )}
-    )}
-    if (sort === 'inprogress' && list) {
-      list.sort((a, b) => {
-        if ( a.taskCount.inprogress === b.taskCount.inprogress ) {
-        return 0;
-        }
-      return( ( a.taskCount.inprogress < b.taskCount.inprogress ) ? -1 : 1 )}
-    )}
-    if (sort === 'done' && list) {
-      list.sort((a, b) => {
-        if ( a.taskCount.done === b.taskCount.done ) {
-        return 0;
-        }
-      return( ( a.taskCount.done < b.taskCount.done ) ? -1 : 1 )}
-    )}
+    }
     return list;
   }
 
