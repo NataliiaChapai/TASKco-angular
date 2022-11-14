824522ff6860c2eb9b3f1a1196bab23899f9258c
Nataliia Chapai <natalya.chapay@gmail.com>
2022-11-14 18:34:43 +0200

    tests sort pipe

--- src/app/shared/pipes/sort/sort.pipe.spec.ts
+++ src/app/shared/pipes/sort/sort.pipe.spec.ts
@@ -1,8 +1,45 @@
+import { BOARDS } from 'src/db-test-data';
 import { SortPipe } from './sort.pipe';
 
 describe('SortPipe', () => {
+
+  let pipe: SortPipe;
+
+  beforeEach(() => {
+    pipe = new SortPipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new SortPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return board sorted by name the with the ascending direction', () => {
+    const sort = 'name'
+    const boards = [...BOARDS];
+    const sortedList = boards.sort((a, b) => {
+      if (a[sort] === b[sort]) {
+        return 0;
+      }
+      return a[sort] < b[sort] ? -1 : 1;
+    })
+    expect(pipe.transform(BOARDS, sort, 'asc')).toEqual(sortedList);
+  });
+
+  it('should return board sorted by date the with the descending direction', () => {
+    const sort = 'createdAt';
+    const boards = [...BOARDS];
+    const sortedList = boards.sort((a, b) => {
+      if (a[sort] === b[sort]) {
+        return 0;
+      }
+      return b[sort] < a[sort] ? -1 : 1;
+    })
+    expect(pipe.transform(BOARDS, sort, 'desc')).toEqual(sortedList);
+  });
+
+  it('first item should have 633c570cdfc7dbef15ca0258 id after sorting by todo task count with the descending direction', () => {
+    const sort = 'todo';
+    const sortedList = pipe.transform(BOARDS, sort, 'desc') ?? BOARDS;
+    expect(sortedList[0]._id).toEqual('633c570cdfc7dbef15ca0258');
   });
 });
