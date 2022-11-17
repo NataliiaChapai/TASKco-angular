e55e747de4a0fcec01885d0295feeac2173e7bbc
Nataliia Chapai <natalya.chapay@gmail.com>
2022-11-18 01:33:03 +0200

    bugfix

--- src/app/features/board/components/task-column/task-column.component.ts
+++ src/app/features/board/components/task-column/task-column.component.ts
@@ -52,7 +52,7 @@ export class TaskColumnComponent implements OnInit {
   }
 
   changeColor(column: string, color: any,) {
-    this.store.changeColor(column, color.target.value).subscribe();
+    this.store.changeColor(column.toLowerCase(), color.target.value).subscribe();
   }
 
   drop(ev: DragEvent) {
