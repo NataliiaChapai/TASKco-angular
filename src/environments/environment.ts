188befc88a9c3415dfa5b51a11732ca06591a926
Nataliia Chapai <natalya.chapay@gmail.com>
2022-12-03 16:26:25 +0200

    change backend deploy from Heroku to Render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://taskco.herokuapp.com/api'
+  // apiUrl: 'https://taskco.herokuapp.com/api'
+  apiUrl: 'https://taskco.onrender.com/api'
 };
 
 /*
